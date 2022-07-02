e40d84dc593edc411eeb2956320113645d7b939a
celsorodrigo <celsorodrigoma@gmail.com>
2022-07-01 22:55:47 -0300

    Concluído 2° requisito.

--- src/App.js
+++ src/App.js
@@ -7,17 +7,74 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      login: '',
+      loguedIn: false,
+      invalidLogin: true,
+    };
+
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.isMount = true;
+  }
+
+  componentWillUnmount() {
+    this.isMount = false;
+  }
+
+  handleLogin({ target }) {
+    const { value } = target;
+    this.setState({
+      login: value,
+      invalidLogin: (value.length <= 2),
+    });
+  }
+
+  handleClick() {
+    const { login } = this.state;
+    this.setState({ loading: true }, async () => {
+      await createUser({ name: login });
+      if (this.isMount) {
+        this.setState({ loading: false, loguedIn: true });
+      }
+    });
+  }
+
   render() {
+    const {
+      login,
+      loading,
+      invalidLogin,
+      loguedIn,
+    } = this.state;
+
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/search" component={ Search } />
-          <Route exact path="/album/:id" component={ Album } />
-          <Route exact path="/favorites" component={ Favorites } />
-          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/">
+            <Login
+              loading={ loading }
+              login={ login }
+              handleLogin={ this.handleLogin }
+              invalidLogin={ invalidLogin }
+              handleClick={ this.handleClick }
+              loguedIn={ loguedIn }
+            />
+          </Route>
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
           <Route exact path="/profile/edit" component={ ProfileEdit } />
           <Route path="*" component={ NotFound } />
         </Switch>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Loading from './Loading';
 
 class Login extends Component {
   render() {
+    const {
+      loading,
+      handleClick,
+      handleLogin,
+      invalidLogin,
+      loguedIn,
+    } = this.props;
+
+    const loginPage = () => (
+      <form>
+        <label htmlFor="name-input">
+          <p>Nome de usuário:</p>
+          <input
+            id="name-input"
+            type="text"
+            data-testid="login-name-input"
+            onChange={ handleLogin }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ invalidLogin }
+          onClick={ handleClick }
+        >
+          Entrar
+        </button>
+      </form>
+    );
+
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        { loguedIn && <Redirect to="/search" /> }
+        { loading
+          ? <Loading />
+          : loginPage() }
+
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  handleLogin: PropTypes.func.isRequired,
+  invalidLogin: PropTypes.bool.isRequired,
+  loguedIn: PropTypes.bool.isRequired,
+};
 
 export default Login;
